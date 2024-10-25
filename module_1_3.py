6b58fada0bc67bc2e16c8fee8b54e637617129ff
aleksey <i@pafosbaby.ru>
2024-10-26 00:04:17 +0700

    Практическая работа по уроку "Динамическая типизация"

--- module_1_3.py
+++ module_1_3.py
@@ -1,8 +1,7 @@
-from math import trunc
-
 name = 'Aleksey'
 age = 24
 new_age = 25
 is_student = True
+
 
 print("Name: " + name + '\n' + "Age: " + str(age) + '\n' + "New Age: " + str(new_age) + '\n' +  "is_student: " + str(is_student))
