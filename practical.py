84110ba8a8100ce4a7692630829637f46f92bd8d
Aleksey <i@pafosbaby.ru>
2024-11-01 19:00:33 +0700

    Дополнительное практическое задание по модулю

---
+++ practical.py
@@ -0,0 +1,24 @@
+# grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+# students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+# print(grades)
+# students_sort = sorted(students)
+# print(students_sort)
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students_sort = sorted(students)
+
+def calculate_average(grades, students_sort):
+    averages = {}
+    for i in range(len(grades)):
+        total = sum(grades[i])
+        average = total / len(grades[i])
+        averages[students_sort[i]] = average
+    return averages
+
+averages = calculate_average(grades, students_sort)
+print(averages)
+
+for students_sort, average in averages.items():
+    print(f'{students_sort}: {average}')
+
+
