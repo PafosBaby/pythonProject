6b47cb9ff095fe062ce86f4bde09158a72a663b0
Aleksey <i@pafosbaby.ru>
2024-11-01 19:01:15 +0700

    Дополнительное практическое задание по модулю

--- practical.py
+++ practical.py
@@ -1,8 +1,4 @@
-# grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-# students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-# print(grades)
-# students_sort = sorted(students)
-# print(students_sort)
+
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students_sort = sorted(students)
